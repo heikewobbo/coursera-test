7529c3ec7123318928787630ba51f4b513ba7841
heikewobbo <fasanom99@gmail.com>
2023-08-23 16:39:40 +0200

    main changes
    new feature!

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const nextPageButton = document.getElementById("next-page");
   const dateTimeElement = document.querySelector(".date-time");
   const filterInput = document.getElementById("filter-input");
+  const rowsPerPageSelect = document.getElementById("rows-per-page"); // Aggiunto
 
-  const rowsPerPage = 10;
   let currentPage = 0;
   let data = [];
+  let rowsPerPage = parseInt(rowsPerPageSelect.value); // Aggiunto
 
   function getCurrentDateTime() {
     const now = new Date();
@@ -18,10 +19,10 @@ document.addEventListener("DOMContentLoaded", function () {
   dateTimeElement.textContent = getCurrentDateTime();
 
   function loadData(callback) {
-    fetch("/coursera-test/airports.dat")
+    fetch("/airports.dat")
       .then(response => response.text())
-      .then(data => {
-        const lines = data.trim().split("\n");
+      .then(rawData => {
+        const lines = rawData.trim().split("\n");
         data = lines.map(line => {
           const [icao, long, lat, name] = line.split("/");
           return { icao, long, lat, name };
@@ -31,15 +32,16 @@ document.addEventListener("DOMContentLoaded", function () {
       .catch(error => console.error("Errore nel caricamento dei dati:", error));
   }
 
-  function updateTable() {
+  function updateTable(dataToDisplay) {
+    // Aggiornato per utilizzare la variabile rowsPerPage
     const start = currentPage * rowsPerPage;
     const end = start + rowsPerPage;
 
-    const dataToDisplay = data.slice(start, end);
+    const dataToRender = dataToDisplay.slice(start, end);
     const tbody = document.getElementById("data-table").querySelector("tbody");
     tbody.innerHTML = "";
 
-    dataToDisplay.forEach(item => {
+    dataToRender.forEach(item => {
       const row = createRow(item);
       tbody.appendChild(row);
     });
@@ -49,14 +51,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function createRow(item) {
     const row = document.createElement("tr");
-    row.innerHTML = `
-      <td>${item.icao}</td>
-      <td>${item.long}</td>
-      <td>${item.lat}</td>
-      <td>${item.name}</td>
-    `;
+    const icaoCell = document.createElement("td");
+    const icaoLink = document.createElement("a");
+    icaoLink.href = `http://www.simbrief.com/system/dispatch.php?dest=${item.icao}`;
+    icaoLink.target = "_blank"; // Apre il link in una nuova finestra/tab
+    icaoLink.textContent = item.icao;
+    icaoLink.classList.add("simbrief-link"); // Aggiungi la classe CSS
+    icaoCell.appendChild(icaoLink);
+    
+    const otherCells = document.createElement("td");
+    otherCells.textContent = item.long;
+    
+    const otherCells2 = document.createElement("td");
+    otherCells2.textContent = item.lat;
+    
+    const otherCells3 = document.createElement("td");
+    otherCells3.textContent = item.name;
+    
+    row.appendChild(icaoCell);
+    row.appendChild(otherCells);
+    row.appendChild(otherCells2);
+    row.appendChild(otherCells3);
+    
     return row;
   }
+  
 
   function updatePaginationButtons() {
     prevPageButton.disabled = currentPage === 0;
@@ -66,27 +85,41 @@ document.addEventListener("DOMContentLoaded", function () {
   prevPageButton.addEventListener("click", () => {
     if (currentPage > 0) {
       currentPage--;
-      updateTable();
+      filterInput.value = ""; // Resetta la barra di ricerca
+      updateTable(data);
     }
   });
 
   nextPageButton.addEventListener("click", () => {
     if (currentPage < Math.ceil(data.length / rowsPerPage) - 1) {
       currentPage++;
-      updateTable();
+      filterInput.value = ""; // Resetta la barra di ricerca
+      updateTable(data);
     }
   });
 
   filterInput.addEventListener("input", () => {
     const filterText = filterInput.value.trim().toLowerCase();
+  
     const filteredData = data.filter(item => 
-      item.icao.toLowerCase().includes(filterText) || item.name.toLowerCase().includes(filterText)
+      (item.icao && item.icao.toLowerCase().includes(filterText)) || 
+      (item.name && item.name.toLowerCase().includes(filterText))
     );
+  
+    currentPage = 0;
     updateTable(filteredData);
   });
+  
 
   loadData(data => {
     data = data;
-    updateTable();
+    updateTable(data);
   });
+
+  rowsPerPageSelect.addEventListener("change", () => {
+    rowsPerPage = parseInt(rowsPerPageSelect.value);
+    currentPage = 0;
+    updateTable(data);
+  });
+
 });
